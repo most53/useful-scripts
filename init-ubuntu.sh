f807fe93a6995e73125748d882ecdcf1093a418f
Max Ostryzhko <maxostryzhko@gmail.com>
2019-05-21 14:49:52 +0300

    Create init-ubuntu.sh

---
+++ init-ubuntu.sh
@@ -0,0 +1,8 @@
+#apt-get update
+apt update
+
+#git, zshell, midnight commander, interactive process viewer
+apt install -y git zsh mc htop
+
+#oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
